7aa7cfd628442bb739f96206ab072d8d5e363329
Moe ASATO <e205747@ie.u-ryukyu.ac.jp>
2020-12-24 00:34:13 +0900

    3rd commit

--- src/Report5.java
+++ src/Report5.java
@@ -1,7 +1,8 @@
 public class Report5{
     public static void main(String[] args){
         try{
-            String str = null;
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
             System.out.println(str.length());
         }catch (NullPointerException e){
             System.out.println("例外：NullPointerExceptionが発生しました。");
